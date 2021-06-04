256c12bf626bbcc59878eac853452d758d85f0a8
mohamedsameh-beep <ms627673@gmail.com>
2021-06-05 00:30:49 +0200

    ADD

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -26,4 +26,48 @@ $(document).ready(function(){
     $(this).toggleClass("active").parent().find(".inputs").slideToggle(500);
   });
 });
+function existingTag(text)
+{
+	var existing = false,
+		text = text.toLowerCase();
 
+	$(".tags").each(function(){
+		if ($(this).text().toLowerCase() == text) 
+		{
+			existing = true;
+			return "";
+		}
+	});
+
+	return existing;
+}
+
+$(function(){
+  $(".tags-new input").focus();
+  
+  $(".tags-new input").keyup(function(){
+
+		var tag = $(this).val().trim(),
+		length = tag.length;
+
+		if((tag.charAt(length - 1) == ',') && (tag != ","))
+		{
+			tag = tag.substring(0, length - 1);
+
+			if(!existingTag(tag))
+			{
+				$('<li class="tags"><span>' + tag + '</span><i class="fa fa-times"></i></i></li>').insertBefore($(".tags-new"));
+				$(this).val("");	
+			}
+			else
+			{
+				$(this).val(tag);
+			}
+		}
+	});
+  
+  $(document).on("click", ".tags i", function(){
+    $(this).parent("li").remove();
+  });
+
+});
